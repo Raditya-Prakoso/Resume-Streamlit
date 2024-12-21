5bbea171056f06bd4b657581d150dfca69d1176c
Raditya-Prakoso <raditya.prakos@gmail.com>
2024-12-21 13:18:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 from PIL import Image
 
 def main_page():
-    st.title("Curriculum Vitae")
+    st.title("📑 Curriculum Vitae")
 
     # Foto
     image = Image.open("assets/profile.png")
-    st.image(image, caption="Muhammad Raditya Prakoso", width=200)
+    st.image(image, width=200)
 
     # Biodata
     st.subheader("Biodata")
@@ -37,24 +37,24 @@ def main_page():
     st.subheader("Work Experiences")
     st.write(
         """
-        - **Direktorat Pengembangan Karir Lulusan dan Hubungan Alumni (DPKHA) Universitas Indonesia** - Data Analyst (February 2024 - Augustus 2024)
+        - **Direktorat Pengembangan Karir Lulusan dan Hubungan Alumni (DPKHA) Universitas Indonesia** - Data Analyst (February 2024 - Augustus 2024) \n
           • Designed and implemented surveys and questionnaires to gather data on alumni employment outcomes, career progression, and educational
-            experiences for Tracer Study (TS) and Employeer Study (ES) for Universitas Indonesia.
+            experiences for Tracer Study (TS) and Employeer Study (ES) for Universitas Indonesia. \n
           • Coordinated and administered online and offline surveys to a diverse alumni population, ensuring high response rates through effective follow-up
-            and engagement strategies.
+            and engagement strategies. \n
           • Analyzed quantitative and qualitative data to assess employment trends, skill utilization, and overall satisfaction among graduates, producing
-            comprehensive reports for stakeholders.
+            comprehensive reports for stakeholders. \n
           • Compiled and presented findings to university administration and academic departments, providing actionable insights to enhan ce
-            curriculum development and student support services.
-        - **NOICE** - User Experience Researcher (February 2023 - Augustus 2023)
+            curriculum development and student support services. \n
+        - **NOICE** - User Experience Researcher (February 2023 - Augustus 2023) \n
           • Developed and implemented comprehensive research workflows utilizing quantitative and qualitative methodologies, with a focus
-            on behavioral and attitudinal analysis.
+            on behavioral and attitudinal analysis. \n
           • Conducted thorough desk research to support primary research initiatives and performed benchmarking and competitor analysis to identify
-            strategic gaps in products and services.
-          • Led market research and usability testing efforts to evaluate product trends, market viability, and user experience.
+            strategic gaps in products and services. \n
+          • Led market research and usability testing efforts to evaluate product trends, market viability, and user experience. \n
           • Managed and analyzed Customer Satisfaction Surveys (CSAT) and Net Promoter Scores (NPS) to inform product development and assess
-            customer satisfaction levels.
-          • Facilitated Focus Group Discussions (FGD) and In-Depth Interviews (IDI) to gather in-depth qualitative insights.
+            customer satisfaction levels. \n
+          • Facilitated Focus Group Discussions (FGD) and In-Depth Interviews (IDI) to gather in-depth qualitative insights. \n
           • Designed survey instruments and conducted advanced data analysis using Microsoft Excel and SPSS.
         """
     )
@@ -63,16 +63,16 @@ def main_page():
     st.subheader("Organisational Experience")
     st.write(
         """
-        - **BebanBerganda** - Think Tank Caretakers – Head of Research and Marketing Strategy (May 2024 - Present)
+        - **BebanBerganda** - Think Tank Caretakers – Head of Research and Marketing Strategy (May 2024 - Present) \n
           Initiating research process and developing the marketing strategy for BebanBerganda, a youth-led community nurturing young leaders with a strong
           commitment to social inclusivity by addressing structural issues and creating systemic impact. BebanBerganda is incubated by Young Leaders for
-          Indonesia, an initiative of McKinsey & Company Indonesia
-        - **Badan Eksekutif Mahasiswa (BEM) FTUI** - Coordinator of Internal Corridor (January 2023 - February 2024)
+          Indonesia, an initiative of McKinsey & Company Indonesia \n
+        - **Badan Eksekutif Mahasiswa (BEM) FTUI** - Coordinator of Internal Corridor (January 2023 - February 2024) \n
           • Supervised three key divisions—Secretary, Organizational Research and Development, and Human Resources Research and Development—
-            overseeing 10 division heads and a team of 35 staff members.
-          • Managed the execution of 15 primary programs and 16 secondary events, driving continuous organizational improvement.
-          • Actively participated on the deliberative board alongside the Chair and Deputy Chair, contributing to all major decisions impacting BEM FTUI.
-          • Designed and implemented a strategic long-term development plan to guide the ongoing enhancement of BEM FTUI.
+            overseeing 10 division heads and a team of 35 staff members. \n
+          • Managed the execution of 15 primary programs and 16 secondary events, driving continuous organizational improvement. \n
+          • Actively participated on the deliberative board alongside the Chair and Deputy Chair, contributing to all major decisions impacting BEM FTUI. \n
+          • Designed and implemented a strategic long-term development plan to guide the ongoing enhancement of BEM FTUI. \n
           • Established a Data Center to monitor and evaluate organizational progress effectively.
         """
     )
@@ -89,16 +89,16 @@ def main_page():
     )
 
 def about_page():
-    st.title("Tentang Aplikasi")
+    st.title("About Application")
     st.write(
         """
-        Aplikasi ini dibuat dengan Streamlit untuk menampilkan informasi Curriculum Vitae.
+        Aplikasi ini dibuat dengan Streamlit untuk menampilkan informasi Curriculum Vitae sebagai pemenuhan tugas Portofolio 5 SanberCode.
         """
     )
 
 # Navigasi multipage
-st.sidebar.title("Navigasi")
-page = st.sidebar.radio("Pilih Halaman", ["Curriculum Vitae", "Tentang Aplikasi"])
+st.sidebar.title("📌 Navigation")
+page = st.sidebar.radio("Pilih Halaman", ["📄 Curriculum Vitae", "⚙️ About Application"])
 
 if page == "Curriculum Vitae":
     main_page()
